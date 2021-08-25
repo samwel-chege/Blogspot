4a07a8c99910197b1d91871640810645244e0d77
samwel-chege <samwelchegeh09@gmail.com>
2021-08-25 23:08:39 +0300

    create index route

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,29 @@
 from flask import render_template,request,redirect,url_for,abort,flash
 from flask_login import login_required,current_user
 from . import main
-from .. models import  User,Comments
-# from .forms import CategoryForm, UpdateProfile,PitchForm,CommentForm
+from .. models import  MailList, User,Comments,Post
+from .forms import SubscribeForm, UpdateProfile,PostForm,CommentForm
 from .. import db,photos
+from ..request import get_quotes,repeat_get_quotes
 
-
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
     '''
     view root page that returns the index page and its data
     '''
     title = 'qoute_blogspot'
-    return render_template('index.html',title = title)
+    quote = get_quotes()
+    quotes = repeat_get_quotes(10, get_quotes)
+    all_posts =Post.get_posts()
+    posts = all_posts.paginate(per_page = 6)
+    form = SubscribeForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+        new_email = MailList(email = email)
+
+        db.session.add(new_email)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+
+    return render_template('index.html',title = title,posts = posts,quotes = quotes,subscribe_form = form)

879222ccb02656d9529a251f41c348ffb580e351
samwel-chege <samwelchegeh09@gmail.com>
2021-08-26 21:50:34 +0300

    import mail password and username

--- config.py
+++ config.py
@@ -8,7 +8,14 @@ class Config:
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-    
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 class ProdConfig(Config):
     '''
     Production  configuration child class

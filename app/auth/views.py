7f3098c888f750ed90542b922b5689fdb171901b
samwel-chege <samwelchegeh09@gmail.com>
2021-08-26 21:47:14 +0300

    change mail message

--- app/auth/views.py
+++ app/auth/views.py
@@ -40,7 +40,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to watchlist","email/welcome_user",user.email,user = user)
+        mail_message("Welcome to Quotes_blogspot","email/welcome_user",user.email,user = user)
 
         return redirect(url_for('auth.login'))
 

2347b8c0d6f2b0d8d00bb35db36955169e65d08e
samwel-chege <samwelchegeh09@gmail.com>
2021-08-26 17:28:40 +0300

    create UPLOADED_PHOTS_DEST configuration to specify image storage

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sammie:samm@localhost/blog'
     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/quotes.json'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
     
